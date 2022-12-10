340dff74156d230cf40af21bec90b6789e74694d
Rogerio Tadeu dos Reis <rogerio.rtr46@gmail.com>
2022-12-10 19:30:46 -0300

    Primeiro commit

---
+++ src/main/java/com/rogerioreis/desafio/DesafioApplication.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+}
