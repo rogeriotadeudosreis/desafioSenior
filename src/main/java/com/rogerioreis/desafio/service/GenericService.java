33f00d3396ca9a4c12cfd2d2be64b5152c4549b1
Rogerio Tadeu dos Reis <rogerio.rtr46@gmail.com>
2023-01-04 07:38:06 -0300

    Implementando generics na aplicação

---
+++ src/main/java/com/rogerioreis/desafio/service/GenericService.java
@@ -0,0 +1,51 @@
+package com.rogerioreis.desafio.service;
+
+import com.rogerioreis.desafio.exception.RecursoNaoEncontradoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GenericService<T, ID> {
+
+    @Autowired
+    JpaRepository<T, ID> getRepository();
+
+    default T create(T anyEntity) {
+        anyEntity = getRepository().save(anyEntity);
+        return anyEntity;
+    }
+
+    default T readById(ID id) {
+        return getRepository().findById(id).orElseThrow(
+                () -> new RecursoNaoEncontradoException("[" + id + "] não encontrado."));
+    }
+
+    default List<T> read() {
+        List<T> list = getRepository().findAll();
+        return list;
+    }
+
+    default T update(T anyEntity, ID id) {
+        this.readById(id);
+        return getRepository().save(anyEntity);
+    }
+
+    default void delete(ID id) {
+        T anyEntity = readById(id);
+        this.getRepository().delete(anyEntity);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
